05dd257ff22dafc6d349d95cf57efbeb837d53b0
priyadarshu <priyadarshinichettiar@gmail.com>
2021-01-13 16:06:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`
+## output
+![](dartop.PNG)
+
+Created from templates made available by Stagehand under a BSD-style
+[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
---
+++ bin/dart_sort_application.dart
@@ -0,0 +1,12 @@
+void main() {
+  List challenges = [
+    'Join our Opening Ceremony',
+    'Set Up a Personal GitHub Page',
+    'Build with a Starter Pack',
+    'Light - Recreate LHD: Build Site',
+    'Basics of Blockchain with Horizen',
+  ];
+
+  challenges.sort();
+  print(challenges);
+}
